2b62371a1837e50e2306b1f7018ff7fa803e6079
Gokula1203 <gokula1203@gmail.com>
2024-07-04 17:56:51 +0530

    tasky

---
+++ index.js
@@ -0,0 +1,28 @@
+const state ={
+taskList:[
+    {
+ type:"",
+ title:"",
+ImageUrl:"",
+Description:""
+    },
+    {
+        type:"",
+        title:"",
+       ImageUrl:"",
+       Description:""
+           },
+           {
+            type:"",
+            title:"",
+           ImageUrl:"",
+           Description:""
+               }
+
+]
+}
+// DOM for Cards
+var cardsContent = document.querySelector(".task_content");
+var cardsView = document.querySelector(".task_view");
+console.log(cardsContent);
+console.log(cardsView);
